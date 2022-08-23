8891ff5dff9865da6cce83c3c2b05ce67ae2f8bf
FrischWaugh <philippe.jacques2137@gmail.com>
2022-08-23 17:05:01 -0400

    Needed a function as entry point

--- cloud-functions/get-nhl-json/main.py
+++ cloud-functions/get-nhl-json/main.py
@@ -19,9 +19,9 @@ def get_schedule(startDate="2021-11-15", endDate="2021-11-17"):
     return response.json()
 
 
-if __name__ == '__main__':
+def get_nhl_json(self)
     schedule = get_schedule()
     file_name = 'schedule_20211115_20211117.json'
     storage_client = storage.Client()
     storage_client.get_bucket('nhl-wizard-data').blob(file_name).upload_from_string(schedule)
-    print('Success!')
+    return('Success!')
