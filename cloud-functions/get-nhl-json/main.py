86d29b6ce1924352d6113c4c8be06d67aed63c77
FrischWaugh <philippe.jacques2137@gmail.com>
2022-08-23 20:40:58 -0400

    Modified function to create blob

--- cloud-functions/get-nhl-json/main.py
+++ cloud-functions/get-nhl-json/main.py
@@ -3,7 +3,8 @@ import simplejson as json
 from google.cloud import storage
 
 def get_schedule(startDate="2021-11-15", endDate="2021-11-17"):
-    """This function extracts all the NHL games results of games
+    """
+    This function extracts all the NHL games results of games
     played between startDate and endDate.
     
     Args:
@@ -14,15 +15,20 @@ def get_schedule(startDate="2021-11-15", endDate="2021-11-17"):
             The end date of the extraction (format must be YYYY-MM-DD).
 
     Returns: A json file containing the requested information.
-
     """
     response = requests.get('https://statsapi.web.nhl.com/api/v1/schedule?date={}&{}'.format(startDate,endDate))
     return response.json()
 
 
 def get_nhl_json(self):
+    """
+    This function will create json object in google cloud storage.
+    """
     schedule = get_schedule()
     file_name = 'schedule_20211115_20211117.json'
     storage_client = storage.Client()
-    storage_client.get_bucket('nhl-wizard-data').blob(file_name).upload_from_string(schedule)
+    # create a blob
+    blob = storage_client.get_bucket('nhl-wizard-data').blob(file_name)
+    # upload the blob 
+    blob.upload_from_string(data=json.dumps(schedule),content_type='application/json')
     return('Success!')
