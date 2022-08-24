fe7c1f05fa0b0a324c2de630a4968cf18834fcde
FrischWaugh <philippe.jacques2137@gmail.com>
2022-08-24 13:30:24 -0400

    First generation json_to_csv

--- cloud-functions/json-to-csv/main.py
+++ cloud-functions/json-to-csv/main.py
@@ -1,32 +1,44 @@
-import requests
 import simplejson as json
 from google.cloud import storage
 
+def get_json(filename):
+    """
+    This function will get the json object from google cloud storage bucket.
+    """
+    # get the blob
+    blob = storage_client.get_bucket('nhl-wizard-data').get_blob(filename)
+    # load blob using json
+    file_data = json.loads(blob.download_as_string())
+    return file_data
 
+def schedule_to_csv(schedule):
+  """
+  This function gets the schedule json and transforms it to a csv file.
+  Args:
+        schedule: json file
+            JSON file containing .
 
-def fetch_and_write(self):
-	"""This function gets the schedule json and transforms it to a csv file"""
-    yesterday = datetime.now() - timedelta(days=1, hours=5)
-    day = yesterday.strftime("%Y-%m-%d")
-    hockey = get_schedule(day)
-    game_list = []
-    for games in hockey:
-        game_id = str(games['gamePk'])
-        game_date = str(games['gameDate'][0:10])
-        game_type = games['gameType']
-        season = str(games['season'])
-        for team in games['teams']:
-            if team=='home':
-                home_away=str(1)
-            else:
-                home_away=str(0)
-            team_id = str(games['teams'][team]['team']['id'])
-            team_score = str(games['teams'][team]['score'])
-            string = game_id + ',' + game_date + ',' + game_type + ',' + season + ',' + home_away + ',' + team_id + ',' + team_score
-            game_list.append(string)
+    Returns: A json file containing the requested information.
+  """
+  game_list = []
+  for date in schedule['dates']:
+    gameDate = date['date']
+    for games in date['games']:
+      gameID = str(games['gamePk'])
+      gameType = games['gameType']
+      season = str(games['season'])
+      homeScore = str(games['teams']['home']['score'])
+      homeTeamID = str(games['teams']['home']['team']['id'])
+      awayScore = str(games['teams']['away']['score'])
+      awayTeamID = str(games['teams']['away']['team']['id'])
+      string = season + ',' + gameDate + ',' + gameID + ',' + gameType + ',' + homeScore + ',' + homeTeamID + ',' + awayScore + ',' + awayTeamID
+      game_list.append(string)
+  return '\n'.join(game_list)
 
-    content = '\n'.join(game_list)
-    file_name = 'nhl-schedule-daily.csv'
-    storage_client = storage.Client()
-    storage_client.get_bucket('nhl-wizard-data').blob(file_name).upload_from_string(content)
-    return 'Success!'
+def json_to_csv(self):
+  schedule = get_json('schedule_20211115_20211117.json')
+  content = schedule_to_csv(schedule)
+  file_name = 'schedule_20211115_20211117.csv'
+  storage_client = storage.Client()
+  storage_client.get_bucket('nhl-wizard-data').blob(file_name).upload_from_string(content)
+  return 'Success!'
